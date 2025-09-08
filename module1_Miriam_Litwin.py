4e980dc13aac737d54a8f4b85455343b09977672
miriamlitwin <mlitwin@student.touro.edu>
2025-09-07 22:08:51 -0400

    Add files via upload

---
+++ module1_Miriam_Litwin.py
@@ -0,0 +1,6 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 09:44:33) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print ('Miriam Litwin')
+Miriam Litwin
+>>> 
